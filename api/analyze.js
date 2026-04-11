bf8ece45cc1fde366df0257a8ea35806fb741af4
chromalab-1987 <labid.chroma@gmail.com>
2026-04-11 20:54:18 -0300

    Update analyze.js

--- api/analyze.js
+++ api/analyze.js
@@ -63,6 +63,7 @@ export default async function handler(req, res) {
         generationConfig: {
           temperature: 0.2,
           maxOutputTokens: 1000,
+          thinkingConfig: { thinkingBudget: 0
         },
       }),
     });
